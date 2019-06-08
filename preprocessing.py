c75ec76c3208b19d6c2374c65e23dcd7d3ac0355
Bhanot, Karan <k.bhanot@cvent.com>
2019-06-08 15:36:48 +0530

    Complete preprocessing of tweets

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,21 @@ import re
 import nltk
 import numpy as np
 import pandas as pd
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from termcolor import colored
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 # Import datasets
+print("Loading data")
 train_data = pd.read_csv('data/train.csv')
 test_data = pd.read_csv('data/test.csv')
+
+# Setting stopwords
+STOPWORDS = set(stopwords.words('english'))
+STOPWORDS.remove("not")
 
 # Function to expand tweet
 def expand_tweet(tweet):
@@ -18,11 +29,41 @@ def expand_tweet(tweet):
 			expanded_tweet.append(word)
 	return expanded_tweet
 
+# Function to process tweets
+def clean_tweet(data, wordNetLemmatizer, porterStemmer, tdIdfVectorizer, type):
+	data['Clean_tweet'] = data['Tweet']
+	print(colored("Removing user handles starting with @", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].str.replace("@[\w]*","")
+	print(colored("Removing numbers and special characters", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].str.replace("[^a-zA-Z' ]","")
+	print(colored("Removing urls", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].replace(re.compile(r"((www\.[^\s]+)|(https?://[^\s]+))"), "")
+	print(colored("Removing single characters", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].replace(re.compile(r"(^| ).( |$)"), " ")
+	print(colored("Tokenizing", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].str.split()
+	print(colored("Removing stopwords", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].apply(lambda tweet: [word for word in tweet if word not in STOPWORDS])
+	print(colored("Expanding not words", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].apply(lambda tweet: expand_tweet(tweet))
+	print(colored("Lemmatizing the words", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].apply(lambda tweet: [wordNetLemmatizer.lemmatize(word) for word in tweet])
+	print(colored("Stemming the words", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].apply(lambda tweet: [porterStemmer.stem(word) for word in tweet])
+	print(colored("Combining words back to tweets", "yellow"))
+	data['Clean_tweet'] = data['Clean_tweet'].apply(lambda tweet: ' '.join(tweet))
+	return data
+
+# Define processing methods
+wordNetLemmatizer = WordNetLemmatizer()
+porterStemmer = PorterStemmer()
+
 # Pre-processing the tweets
-print(train_data['Tweet'][0])
-train_data['Clean_tweet'] = train_data['Tweet']
-train_data['Clean_tweet'] = train_data['Clean_tweet'].str.replace("@[\w]*","")
-train_data['Clean_tweet'] = train_data['Clean_tweet'].str.replace("[^a-zA-Z' ]","")
-train_data['Clean_tweet'] = train_data['Clean_tweet'].str.split()
-train_data['Clean_tweet'] = train_data['Clean_tweet'].apply(lambda tweet: expand_tweet(tweet))
-print(train_data['Clean_tweet'][0])
+print(colored("Processing train data", "green"))
+train_data = clean_tweet(train_data, wordNetLemmatizer, porterStemmer, tdIdfVectorizer, 'train')
+train_data.to_csv('data/clean_train.csv', index = False)
+print(colored("Train data processed and saved to data/clean_train.csv", "green"))
+print(colored("Processing test data", "green"))
+test_data = clean_tweet(test_data, wordNetLemmatizer, porterStemmer, tdIdfVectorizer, 'train')
+test_data.to_csv('data/clean_test.csv', index = False)
+print(colored("Test data processed and saved to data/clean_test.csv", "green"))
